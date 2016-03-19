1d63e0e514c5db97bd2dc00bbdaf3dd199217b04
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-03-19 16:42:21 +0000

    Accurately generating XOR payloads

--- raw2src.py
+++ raw2src.py
@@ -14,29 +14,19 @@ import sys
 import hashlib
 import argparse
 
-domainhash = hashlib.sha1()
-domainhash.update("TESTER2")
-domainhash_value = domainhash.digest()
-domainhash_size = domainhash.digest_size
-
-# Read the shellcode from shellcode.raw
-with open('shellcode.raw', 'rb') as s:
-    shellcode = s.read()
-    s.close()
-
-def writeout(varname, data, xorval, fmt):
+def writeout(varname, data, xorval, xorsize, fmt):
     hash_counter = 0
     shellcode_counter = 0
     sys.stdout.write(varname+" db ")
-    for c in shellcode:
+    for c in data:
         if shellcode_counter % 20:
             sys.stdout.write(',')
         elif shellcode_counter:
-            sys.stdout.write("\n" + " "*varname.length() + "db ")
+            sys.stdout.write("\n" + " "*len(varname) + " db ")
     
         if xorval != None:
             original_opcode = c
-            new_opcode = ord(c) ^ ord(domainhash_value[hash_counter])
+            new_opcode = ord(c) ^ ord(xorval[hash_counter])
         else:
             new_opcode = ord(c)
         sys.stdout.write(str(new_opcode))
@@ -44,32 +34,55 @@ def writeout(varname, data, xorval, fmt):
         shellcode_counter += 1
 
         if xorval != None:
-            if hash_counter==domainhash_size - 1:
+            if hash_counter==xorsize - 1:
                 hash_counter = 0
             else:
                 hash_counter = hash_counter + 1
 
     sys.stdout.write("\n")
-    sys.stdout.write("shellcodelen  equ  "+str(shellcode_counter)+"\n")
+    sys.stdout.write(varname+"len  equ  "+str(shellcode_counter)+"\n")
+    sys.stdout.write("\n")
 
 def generate_computername_hash(computername, outformat):
     computernamehash = hashlib.sha1()
     computernamehash.update(computername)
-    computernamehash_value = domainhash.digest()
-    writeout("hashSHA1CompterName", computernamehash_value, None, outformat)
+    computernamehash_value = computernamehash.digest()
+    sys.stdout.write("# This is the hash of: "+computername+"\n")
+    writeout("hashSHA1ComputerName", computernamehash_value, None, None, outformat)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Shellcode to C/ASM implant stub converter.')
     parser.add_argument('-c', '--computername', action='store', help='Generate the SHA1 hash of the parameter given (e.g. a computer name)')
     parser.add_argument('-x', '--xor', action='store', help='XOR the shellcode with the hash of the parameter given (e.g. domain name)')
-    parser.add_argument('-s', '--shellcode', action='store', help='The filename containing the shellcode. Specify "-" to read from STDIN')
-    parser.add_argument('-o', '--output-format', action='store', help='The output format. Can be "C" or "MASM"')
+    parser.add_argument('-s', '--shellcode', action='store', help='The filename containing the shellcode.')
+    parser.add_argument('-o', '--outputformat', action='store', help='The output format. Can be "C" or "MASM"')
     args = vars(parser.parse_args())
 
-    if 'output-format' in args and args['output-format'] != None:
-        if args['output-format'] != 'C' and args['output-format'] != 'MASM':
+    if 'outputformat' in args and args['outputformat'] != None:
+        if args['outputformat'] != 'C' and args['outputformat'] != 'MASM':
             sys.stderr.write("Invalid output format\n")
             sys.exit(1)
+    else:
+        sys.stderr.write("Invalid output format\n")
+        sys.exit(1)
     
     if 'computername' in args and args['computername'] != None:
-        
+        generate_computername_hash(args['computername'], args['outputformat'])
+
+    if 'shellcode' in args and args['shellcode'] != None:
+        # Read the shellcode from shellcode.raw
+        with open(args['shellcode'], 'rb') as s:
+            shellcode = s.read()
+            s.close()
+        sys.stdout.write("# Shellcode loaded from: "+args['shellcode']+"\n")
+
+        xor = None
+        xorsize = None
+        if 'xor' in args and args['xor'] != None and len(args['xor'])>0:
+            domain_xor = hashlib.sha1()
+            domain_xor.update(args['xor'])
+            xor = domain_xor.digest()
+            xorsize = domain_xor.digest_size
+            sys.stdout.write("# Shellcode XOR'd with hash of: "+args['xor']+"\n")
+
+        writeout('shellcode', shellcode, xor, xorsize, args['outputformat'])
