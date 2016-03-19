2210eb6eaa1a11b6bb9e749c85fb9bfb5f4e1df9
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-03-19 16:09:22 +0000

    Tidy up repo

--- stub-masm-dll/makeit.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-if exist stub-dll.obj del stub-dll.obj
-if exist stub-dll.dll del stub-dll.dll
-\masm32\bin\ml /c /coff stub-dll.asm
-\masm32\bin\Link /SUBSYSTEM:WINDOWS /DLL /DEF:stub-dll.def stub-dll.obj 
-del stub-dll.obj
-del stub-dll.exp
-dir stub-dll.*
-pause
